c7d295fe715f96f83b66560e9c6e62da1a4e0809
Mastoora Turkmen <janmastoorajan@gmail.com>
2026-01-02 22:05:11 +0430

    testing files for header, footer, page, single and index

--- wp-content/themes/fictional-university-theme/index.php
+++ wp-content/themes/fictional-university-theme/index.php
@@ -1,15 +1,16 @@
-<?php 
-    function greet($name, $color){
-        echo "<p>Hi, my name is $name and my favorite color is $color</p>";
+<?php get_header();
+
+    while(have_posts()){
+        the_post(); ?>
+        <h2>
+            <a href="<?php the_permalink(); ?>">
+                <?php the_title(); ?> 
+            </a>
+        </h2>
+        <p><?php the_content(); ?></p>
+        <hr>
+        <?php
     }
 
-    greet("Mastoora Turkmen", "blue"); 
-    greet("Aseel Alwash", "green"); 
+    get_footer();
 ?>
-
-<h1>
-    <?php bloginfo('name'); ?>
-</h1>
-<p>
-    <?php bloginfo('description'); ?>
-</p>
